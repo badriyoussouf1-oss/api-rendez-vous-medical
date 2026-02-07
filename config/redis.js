c271c5f83970cf34f6791b8771f45bd8a868d8a7
badri youssouf <badriyoussouf1@gmail.com>
2026-02-07 17:52:26 +0000

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -1,13 +1,9 @@
 const redis = require('redis');
 require('dotenv').config();
-
-
 const redisClient = redis.createClient({
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: process.env.REDIS_PORT || 6379
 });
-
-
 redisClient.on('error', (err) => {
   console.error('Erreur de connexion à Redis:', err);
 });
@@ -15,7 +11,6 @@ redisClient.on('error', (err) => {
 redisClient.on('connect', () => {
   console.log('Connexion à Redis établie avec succès.');
 });
-
 
 (async () => {
   try {
