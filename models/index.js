e57e152af9d691cf7e6b0ab320932d9ff1c7c5e3
badri youssouf <badriyoussouf1@gmail.com>
2026-02-07 17:50:28 +0000

    Refactor index.js by cleaning up comments and whitespace
    Removed comments and unnecessary whitespace, cleaned up the syncDatabase function.

--- models/index.js
+++ models/index.js
@@ -1,16 +1,10 @@
 const sequelize = require('../config/database');
-
-// Importer tous les modèles
 const Admin = require('./Admin');
 const Secretaire = require('./Secretaire');
 const Docteur = require('./Docteur');
 const Patient = require('./Patient');
 const RendezVous = require('./RendezVous');
 
-
-
-// Relation Patient <-> RendezVous (1:N)
-// Un patient peut avoir plusieurs rendez-vous
 Patient.hasMany(RendezVous, {
   foreignKey: 'patient_id',
   as: 'rendezVous'
@@ -21,8 +15,6 @@ RendezVous.belongsTo(Patient, {
   as: 'patient'
 });
 
-// Relation Docteur <-> RendezVous (1:N)
-// Un docteur peut avoir plusieurs rendez-vous
 Docteur.hasMany(RendezVous, {
   foreignKey: 'docteur_id',
   as: 'rendezVous'
@@ -33,21 +25,11 @@ RendezVous.belongsTo(Docteur, {
   as: 'docteur'
 });
 
-// ============================================
-// SYNCHRONISATION DE LA BASE DE DONNÉES
-// ============================================
-
-// Fonction pour synchroniser tous les modèles avec la base de données
 const syncDatabase = async (options = {}) => {
   try {
-    // options.force = true : Supprime et recrée toutes les tables (ATTENTION : efface les données)
-    // options.alter = true : Modifie les tables existantes pour correspondre aux modèles
-    // Par défaut (sans options) : Crée les tables si elles n'existent pas
-    
     await sequelize.sync(options);
     console.log('Base de données synchronisée avec succès !');
-    
-    // Afficher les tables créées
+
     const tables = await sequelize.getQueryInterface().showAllTables();
     console.log('Tables créées :', tables);
     
@@ -56,10 +38,6 @@ const syncDatabase = async (options = {}) => {
     throw error;
   }
 };
-
-// ============================================
-// EXPORT DES MODÈLES ET FONCTIONS
-// ============================================
 
 module.exports = {
   sequelize,
